a4e66b0faf5986f971870479c5a8ca8136645da6
hanfei <1485731520@qq.com>
2022-10-13 01:20:23 +0800

    docs: login
    coderzlk is login

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id='nav'>
+    <router-link to='/'>Home</router-link>
+    |
+    <router-link to='/about'>About</router-link>
   </div>
-  <router-view/>
+
+  <router-view />
 </template>
 
-<style lang="less">
+<style lang='less'>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
